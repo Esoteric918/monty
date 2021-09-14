9f9c8b07889d4c6521f7036e115b01d58919613f
RockoGilbert <evolve2122@gmail.com>
2021-09-14 17:01:55 -0500

    reverted changes

--- 456op.c
+++ 456op.c
@@ -66,9 +66,8 @@ void add(stack_t **stack, unsigned int line_number)
  * @line_number: number of line opcode occurs on
  */
 
-void _nop(__attribute__ ((unused))stack_t **stack,
-				__attribute__ ((unused)) unsigned int line_number)
+void nop(stack_t **stack, unsigned int line_number)
 {
-		;
-}
-
+	(void)stack;
+	(void)line_number;
+} 
