ab4ab5d390c1d4d32d782ae78b65fe808d52304a
RockoGilbert <evolve2122@gmail.com>
2021-09-14 13:17:09 -0500

    Fixed Betty errors

--- monty.c
+++ monty.c
@@ -2,31 +2,34 @@
 global_var global_variable;
 
 /**
- *  main - entry point
+ * main - entry point
  * @argc: argument count
  * @argv: argument value
  * Description: run the m file
  * Return: 0
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-    char line[80];
-    char *token;
-    char array[2][80];
-    int i = 0;
+	char line[80];
+	char *token;
+	char array[2][80];
+	int i = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    global_variable.file = fopen(argv[1], "r");
-    if (!(global_variable.file))
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
-global_variable.line_number = 1;
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	global_variable.file = fopen(argv[1], "r");
+
+	if (!(global_variable.file))
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	global_variable.line_number = 1;
 	global_variable.stack = NULL;
 	global_variable.queue_ask = 0;
 	while (fgets(line, sizeof(line), global_variable.file))
